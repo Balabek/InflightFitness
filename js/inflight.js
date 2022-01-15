7274182f771e3a033cdc023e5680596c410a4b6e
Balabek <vonbabayan@gmail.com>
2022-01-15 22:49:12 +0300

    Fetus

--- js/inflight.js
+++ js/inflight.js
@@ -1,1 +1,25 @@
 /* 𝙲𝚞𝚜𝚝𝚘𝚖 𝙹𝚂 */
+
+$(document).ready(function() {
+
+    // Testimonials slider
+    $('.t_slider').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplaySpeed: 5000,
+        speed: 400,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+
+
+
+
+
+
+
+
+});
